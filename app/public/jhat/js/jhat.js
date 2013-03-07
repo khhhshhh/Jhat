ba2644c188973ea7375d2250c458c233a37adaac
khhhshhh <iammfw@163.com>
2013-03-08 01:16:23 +0800

    Add inherit method

--- app/public/jhat/js/jhat.js
+++ app/public/jhat/js/jhat.js
@@ -1,51 +1,56 @@
 (function() {
 
-	/*deal with / without jQuery*/
-	var $ = null;
-	if(jQuery) {
-		$ = jQuery;
-	} else {
-		$ = function(selector) {
-		};
-	}
-
-	function errorHandler(msg) {
-		console.log(msg);
-	};
-
 	/*jhat's global configure settings*/
 	var config = {
 		host: 'http://localhost',
 		port: ':8080',
 		style: '/jhat/css/jhat.css',
-		template: /*'/jhat/templates/jhat.html'*/ '/jhat/loadTemplate',
+		template: '/jhat/loadTemplate',
 	};
 	config.url = function() {
 		return this.host + this.port;
 	};
 
-	/*global variables*/
-	var $body = $(document.body);
-	var $style = $('<link>'); // jhat's css style 
-	var $template = null ; // jhat's dom template
 
+	/*deal with or without jQuery*/
+	var $ = null;
+	if(jQuery || Zepto) {
+		$ = jQuery || Zepto;
+	} else {
+		$ = function(selector) {
+		};
+	}
+
+	/* Error handling, I have no idea how to figure out 
+	 * a better way than console it out. 
+	 * */
+	function errorHandler(msg) {
+		console.log(msg);
+	};
+
+	/*global variables*/
+	var $body = $(document.body)
+		,$style = $('<link>') // jhat's css style 
+		,$template = null // jhat's dom template
+		,speed = 500
+		,$dialogue = null; // dialogue template, will init when $template is load
+
+	/*Load style*/
 	$style.attr({
 		'type': 'text/css',
 		'rel': 'stylesheet',
 		'href': config.host + config.port + config.style
 	});
-
-	/*Load style*/
 	$body.append($style);
 
 	/*Load template*/
 	$.ajax({
-		url: config.ur() + config.template,
+		url: config.url() + config.template,
 		type: 'GET',
 		dateType: 'text/html',
 		success: function(data) {
 			$template = $(data);
-			$body.append($template);
+			$dialogue = $template.find('div.jhat-dialogue');
 			init();
 		},  
 		error: function(err) {
@@ -53,39 +58,53 @@
 		}
 	}); 
 
-	function init() {
-		widget();
-	};
+	/*Inherit method*/
+	var inherit = (function(Sup, Sub) {
+		function F() {
+			if(typeof F.prototype.init == 'function') {
+				this.init();
+			}
+		}; 
+		return function(Sup, Sub) {
+			F.prototype = Sup.prototype;
+			Sub.prototype = new F();
+			Sub.uber = Sup;
+			Sub.prototype.contructor = Sup;
+		};
+	})();
 
-	/*Side widget*/
-	function Side() {};
-	Side.fn = Side.prototype;
-	Side.fn = {
-		contructor: 'Side',
-		init: function(){},  
-		$dom: null,
-		show: function(speed) {
-			var speed = speed || 200;
-			this.$dom.animate({
-				right: '5px'
-			}, speed);
+	/*Widget contructor, super class of all widget*/
+	function Widget(options) {};
+	Widget.fn = Widget.prototype;
+	Widget.fn = {
+		contructor: 'Widget',
+		$template: null, 
+		init: function() {
+			/* init the widget*/ 
+			var $widget = this.$dom = this.$template.clone(true);
+			$widget.addClass('jhat-widget');
+			$widget.appendTo($body);
 		},
-		hide: function(speed) {
-			var speed = speed || 200;
-			this.$dom.animate({
-				right: '-45px'
-			}, speed);
+		hide: function() {
+			this.$dom.hide(speed);
+		},
+		show: function() {
+			this.$dom.show(speed);
 		},
 		destory: function() {
 			this.$dom.remove();
 		}
+	};  
+
+	function init() {
+		widget();
 	};
 
 	function widget() {
-		var speed = 200;
-		var $show = $template.filter('div.jhat-show');
-		var $box = $template.filter('#jhat-box');
-		var $hide = $box.find('li.jhat-icon-hide');
+		var speed = 200
+			,$show = $template.filter('div.jhat-show').appendTo($body)
+			,$box = $template.filter('#jhat-box').appendTo($body)
+			,$hide = $box.find('li.jhat-icon-hide');
 
 		$show.extend({
 			display: function() {
@@ -124,11 +143,16 @@
 			$show.disappear();
 		});
 
-		$box.find('#jhat-signature').on('blur', function(event) {
+		/* Update the user's signature. After typing the
+		 * signature, the code coming after aim to send a  
+		 * XHR request to update it in Mongodb database 
+		 * */
+		$box.find('#jhat-signature')
+			.on('blur', function(event) {
 			var $this = $(this);
 			var signature = $this.val();
 
-			$this.attr('');
+			$this.attr('title', signature);
 
 			$.ajax({
 				url: config.url() + '/changeSignature',
@@ -145,6 +169,7 @@
 			});
 		});
 
+		/*The button for hiding the side */
 		$hide.on('click', function(event) {
 			$box.disappear();
 			$show.display();
